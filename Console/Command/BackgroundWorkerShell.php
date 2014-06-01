1ca00e3a51b66ee7c4f2ed192c84c1b894a0a12f
Ivan Stoilov <stoilov.ivan@gmail.com>
2014-06-01 15:57:41 +0200

    Crons + continuous looping
    Crons added to the plugin + continuous looping until all QUEUED jobs
    are done implemented

--- Console/Command/BackgroundWorkerShell.php
+++ Console/Command/BackgroundWorkerShell.php
@@ -13,6 +13,7 @@ class BackgroundWorkerShell extends AppShell {
 			->description(__("Used for managing background workers."))
 			->addSubcommand('start', array('help' => __('Starts a new worker process which will poll for queued jobs every <interval> seconds.')))
 				->addOption('interval', array ('short' => 'i', 'help' => 'Polling interval in seconds.', 'default' => 10, 'boolean' => false))
+				->addOption('type', array ('short' => 't', 'help' => 'Type of worker to start.', 'default' => 'bgjobs', 'boolean' => false, 'choices' => array('bgjobs', 'crons')))
 			->addSubcommand('stop', array('help' => __('Stops one background worker.')))
 			->addSubcommand('status',array('help' => __('Get info about the currently started workers.')))
 			->addSubcommand('stopAll',array('help' => __('Stops all active workers.')));
@@ -21,47 +22,53 @@ class BackgroundWorkerShell extends AppShell {
 	public function runQueued() {
 		$job = $this->Job->reserve();
 
-		if (empty($job)) {
-			// no queued jobs found
-			return;
+		while (!empty($job)) {
+			$this->run($job['Job']['task'], json_decode($job['Job']['args'], 1));
+			$this->Job->markAsFinished($job['Job']['id']);
+			$job = $this->Job->reserve();
 		}
-
-		$this->run($job);
-
-		$this->Job->markAsFinished($job['Job']['id']);
 	}
 
-	public function run($job) {
+	public function runCrons() {
+		Configure::load("crons");
+		$crons = Configure::read("Jobs");
+
+		foreach ($crons as $task => $params) {
+			if (floor((time() / 60)) % $params['every'] === 0) {
+				$passParams = isset($params['params']) && is_array($params['params']) ? $params['params'] : array();
+				$this->run($task, $passParams);
+			}
+		}
+	}
+
+	public function run($task, $params) {
 		try {
-			$params = json_decode($job['Job']['args'], 1);
-
-			$Task = $this->Tasks->load($job['Job']['task']);
+			$Task = $this->Tasks->load($task, $params);
 			$Task->initialize();
-
 			call_user_func_array(array($Task, 'execute'), $params);
 		} catch (Exception $ex) {
-			CakeLog::write('job', "Job failed with exception: {$ex->getMessage()} - " . json_encode($job['Job']));
+			CakeLog::write('job', "Job failed with exception: {$ex->getMessage()} - " . json_encode([$task, $params]));
 		}
 	}
 
 	public function rerun() {
 		$jobId = $this->args[0];
 		$job = $this->Job->findById($jobId);
-		$this->run($job);
+		$this->run($job['Job']['task'], json_decode($job['Job']['args'], 1));
 	}
 
 	public function start() {
-		$cmd = "app/Plugin/BackgroundJobs/Vendor/bgjobs.sh";
-		$logFile = "app/tmp/logs/bgjobs.log";
+		$cmd = "app/Plugin/BackgroundJobs/Vendor/{$this->params['type']}.sh";
+		$logFile = "app/tmp/logs/{$this->params['type']}.log";
 
 		$interval = $this->params['interval'];
 
 		exec(sprintf("%s %s > /dev/null 2> %s &", $cmd, $interval, $logFile), $output, $result);
 
 		if ($result === 0) {
-			$this->out("<info>Worker started (polling interval = {$interval}s)</info>\n");
+			$this->out("<info>Worker ({$this->params['type']}) started (polling interval = {$interval}s)</info>\n");
 		} else {
-			$this->out("<error>Worker failed to start :(</error>");
+			$this->out("<error>Worker ({$this->params['type']}) failed to start :(</error>");
 		}
 	}
 
@@ -102,7 +109,7 @@ class BackgroundWorkerShell extends AppShell {
 			$this->out("<info>Currently started works: ".count($workers)."</info>\n");
 
 			foreach ($workers as $worker) {
-				$this->out("Worker (pid=$worker) is up.");
+				$this->out("Worker (pid=$worker, type={$this->params['type']}) is up.");
 			}
 		} else {
 			$this->out("Currently, no workers are running.");
@@ -112,7 +119,7 @@ class BackgroundWorkerShell extends AppShell {
 	}
 
 	private function _getWorkersPids() {
-		$cmd = "ps -o pid,comm,args | grep bgjobs | grep -v grep";
+		$cmd = "ps -o pid,comm,args | grep {$this->params['type']} | grep -v grep | grep -v cake.php";
 
 		exec($cmd, $output, $result);
 
--- Vendor/crons.sh
+++ Vendor/crons.sh
@@ -4,13 +4,13 @@ INTERVAL=$1
 
 if [ -z $1 ]
 then
-	INTERVAL=10
+	INTERVAL=50
 else
 	INTERVAL=$1
 fi
 
 while [ 1 ]
 do
-	./cake BackgroundJobs.BackgroundWorker runQueued --quiet
+	./cake BackgroundJobs.BackgroundWorker runCrons --quiet
 	sleep $INTERVAL
 done
